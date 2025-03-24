379fafb6106de067118a64e3e5aaaa9449856ba2
sabarish1608 <sabarishlive@gmail.com>
2025-03-25 01:48:15 +0530

    Fixed base path for GitHub Pages 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/CharityCare-Website/',  
+  base: "/CharityCare-Website/",  // ✅ Add your repo name here
 });
+
 
